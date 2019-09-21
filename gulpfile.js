0b417f7b56473a008cc889d5b059592e6679d2de
Stuart Kent <stuart@stuartkent.co.nz>
2019-09-22 07:47:16 +1200

    tidied up, and added some unofficial content, hopefully this helps preserve

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,39 @@ var html2TextOpts = {
     ignoreImage  :true,
 }
 
-gulp.task('default', function() {
-  return gulp.src('./source/*.html')
-    .pipe(concat('merged.html'))
-    .pipe(replace("&nbsp;", " "))
-    .pipe(replace("<p>", "<br/><p>"))
-    .pipe(replace("<ul>", "<br/><ul>"))
-    .pipe(html2txt(html2TextOpts))
-    .pipe(replace(/\]([a-zA-Z0-9]{1})/g, '] $1'))
-    .pipe(replace(/([a-zA-Z0-9]{1})\[/g, '$1 ['))
-    .pipe(gulp.dest('./concat/'));
+var builds = {
+    patchOnly : ["source/official/patches/*.html"],
+    aprilOnly : [
+      "source/official/april_fools/*.html",
+      "source/fansites/april_fools/*.html"
+    ],
+    mordern_speculation: [
+      "source/official/patches/7*.html",
+      "source/official/patches/8*.html",
+    ],    
+    mordern_speculation_funny: [
+      "source/official/patches/7*.html",
+      "source/official/patches/8*.html",
+      "source/official/april_fools/*.html",
+      "source/fansites/april_fools/*.html"
+    ],
+    everything: ["source/**/*.html"]
+    // idk maybe a speculation variant 
+}
+
+var defaultTasks = Object.keys(builds);
+defaultTasks.forEach(function(taskName) {
+   gulp.task(taskName, function() {
+       return gulp.src(jsFiles[taskName])
+       .pipe(concat('merged.html'))
+       .pipe(replace("&nbsp;", " "))
+       .pipe(replace("<p>", "<br/><p>"))
+       .pipe(replace("<ul>", "<br/><ul>"))
+       .pipe(html2txt(html2TextOpts))
+       .pipe(replace(/\]([a-zA-Z0-9]{1})/g, '] $1'))
+       .pipe(replace(/([a-zA-Z0-9]{1})\[/g, '$1 ['))
+       .pipe(gulp.dest('./concat/'));
+   });
 });
 
 
